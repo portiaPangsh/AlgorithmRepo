28a7c0a25d2a593f31d419184c4b29323c9f2e62
Portia Pang <shuhao.pang@cityexperiences.com>
2022-03-09 11:46:22 -0800

    02-addTwoNumber-updates

--- 02-addTwoNumbers.js
+++ 02-addTwoNumbers.js
@@ -1,23 +1,47 @@
-var listSum = function(node) {
-    let res = 0;
-    while (node) {
-        res = res * 10 + node.val;
-        node = node.next;
-    }
-    return res;
-};
+// Wrong!!!!!!!
+// var listSum = function(node) {
+//     let res = '';
+//     while (node) {
+//         res = res + node.val.toString();
+//         node = node.next;
+//     }
+//     return parseFloat(res.split('').reverse().join(''));
+// };
+
+// var addTwoNumbers = function (l1, l2) {
+//     const list1Sum = listSum(l1)
+//     const list2Sum = listSum(l2)
+//     console.log({list1Sum,list2Sum})
+//     let sum = list1Sum + list2Sum;
+//         console.log({sum})
+
+
+//     if(sum === 0) {
+//         return new ListNode(0);
+//     }
+//     let dummyNode = cur = new ListNode();
+//     while (sum > 0){
+//         const num = sum % 10;
+//         cur.next = new ListNode(num);
+//         cur = cur.next;
+//         sum = (sum - num) / 10
+//     }
+//     return dummyNode.next;
+// }
+
 
 var addTwoNumbers = function (l1, l2) {
-    const list1Sum = listSum(l1)
-    const list2Sum = listSum(l2)
-    let sum = list1Sum + list2Sum;
     let dummyNode = cur = new ListNode();
-    while (sum > 0){
-        const num = sum % 10;
-        cur.next = new ListNode(num);
+    let addOne = 0;
+    while(l1 || l2 || addOne) {
+        let v1 = l1 ? l1.val : 0;
+        let v2 = l2 ? l2.val : 0;
+        let sum = v1 + v2 + addOne;
+        cur.next = new ListNode( sum % 10 );
+        addOne = parseInt( sum / 10 )
         cur = cur.next;
-        sum = (sum - num) / 10
+        l1 = l1 && l1.next
+        l2 = l2 && l2.next
     }
-    console.log(dummyNode.next.val);
     return dummyNode.next;
 }
