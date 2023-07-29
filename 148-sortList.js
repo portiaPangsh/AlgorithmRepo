863bc2e8bf04ba58e1f6fd64b27f1eb4b3ab83ca
Portia Pang <shuhao.pang@cityexperiences.com>
2023-07-29 16:47:27 -0700

    148.sortList

---
+++ 148-sortList.js
@@ -0,0 +1,12 @@
+/*
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example A:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example B:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+*/
